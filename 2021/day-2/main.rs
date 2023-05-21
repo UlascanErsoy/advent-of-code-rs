735b661d11ea210962c37f7a8889635930884497
Ulascan Ersoy <cake@pop-os.localdomain>
2023-05-20 23:45:26 -0500

    2021/day-2

---
+++ 2021/day-2/main.rs
@@ -0,0 +1,26 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    
+    let args: Vec<String> = env::args().collect();
+    let content = fs::read_to_string(&args[1])
+                    .unwrap();
+
+    let (mut vert, mut horz) = (0,0);
+
+    for line in content.split("\n") {
+        let v: Vec<_> = line.split(" ").collect();
+        if v.len() == 2 {
+            match (v[0] , v[1].parse().unwrap_or(0)) {
+                ("forward", m) => horz += m,
+                ("down",m) => vert += m,
+                ("up",m) => vert -= m,
+                _ => println!("nah")
+            };
+        }
+    }
+
+    println!("Vertical: {vert}, Horizontal: {horz}");
+    println!("Result: {}", vert * horz);
+}
